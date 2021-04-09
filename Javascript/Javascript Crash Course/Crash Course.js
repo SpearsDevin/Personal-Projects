a6456777d543e7aeed57d3af00d21a7382e36954
Devin Spears <devinspears2004@gmail.com>
2021-04-09 16:29:41 -0500

    New Project

--- Javascript/Javascript Crash Course/Crash Course.js
+++ Javascript/Javascript Crash Course/Crash Course.js
@@ -1,5 +1,16 @@
+        let todo1 = 'Get Groceries';
+        let todo2 = 'wash Car';
+        let todo3 = 'Make Dinner';
 
+        let element = document.createElement('div');
+        element.innerText = todo1;
+        document.body.appendChild(element);
 
-let variablename = "Welcome to Variables";
-console.log(variablename);
+        element = document.createElement('div');
+        element.innerText = todo2;
+        document.body.appendChild(element);
+
+        element = document.createElement('div');
+        element.innerText = todo3;
+        document.body.appendChild(element);
 
